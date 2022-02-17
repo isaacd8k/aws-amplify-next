29b240126b4d70ad4b5a2f33ea9fb80f88c2d80d
Isaac Farciert <rfisaac2296@yahoo.com>
2022-02-17 17:22:27 -0500

    prettier

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "awsamplifynext": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
+  api: {
+    awsamplifynext: {
+      GraphQLAPIKeyOutput: "string";
+      GraphQLAPIIdOutput: "string";
+      GraphQLAPIEndpointOutput: "string";
+    };
+  };
+};
--- src/API.ts
+++ src/API.ts
@@ -3,33 +3,33 @@
 //  This file was automatically generated and should not be edited.
 
 export type CreatePostInput = {
-  id?: string | null,
-  name: string,
-  description?: string | null,
+  id?: string | null;
+  name: string;
+  description?: string | null;
 };
 
 export type ModelPostConditionInput = {
-  name?: ModelStringInput | null,
-  description?: ModelStringInput | null,
-  and?: Array< ModelPostConditionInput | null > | null,
-  or?: Array< ModelPostConditionInput | null > | null,
-  not?: ModelPostConditionInput | null,
+  name?: ModelStringInput | null;
+  description?: ModelStringInput | null;
+  and?: Array<ModelPostConditionInput | null> | null;
+  or?: Array<ModelPostConditionInput | null> | null;
+  not?: ModelPostConditionInput | null;
 };
 
 export type ModelStringInput = {
-  ne?: string | null,
-  eq?: string | null,
-  le?: string | null,
-  lt?: string | null,
-  ge?: string | null,
-  gt?: string | null,
-  contains?: string | null,
-  notContains?: string | null,
-  between?: Array< string | null > | null,
-  beginsWith?: string | null,
-  attributeExists?: boolean | null,
-  attributeType?: ModelAttributeTypes | null,
-  size?: ModelSizeInput | null,
+  ne?: string | null;
+  eq?: string | null;
+  le?: string | null;
+  lt?: string | null;
+  ge?: string | null;
+  gt?: string | null;
+  contains?: string | null;
+  notContains?: string | null;
+  between?: Array<string | null> | null;
+  beginsWith?: string | null;
+  attributeExists?: boolean | null;
+  attributeType?: ModelAttributeTypes | null;
+  size?: ModelSizeInput | null;
 };
 
 export enum ModelAttributeTypes {
@@ -45,180 +45,179 @@ export enum ModelAttributeTypes {
   _null = "_null",
 }
 
-
 export type ModelSizeInput = {
-  ne?: number | null,
-  eq?: number | null,
-  le?: number | null,
-  lt?: number | null,
-  ge?: number | null,
-  gt?: number | null,
-  between?: Array< number | null > | null,
+  ne?: number | null;
+  eq?: number | null;
+  le?: number | null;
+  lt?: number | null;
+  ge?: number | null;
+  gt?: number | null;
+  between?: Array<number | null> | null;
 };
 
 export type Post = {
-  __typename: "Post",
-  id: string,
-  name: string,
-  description?: string | null,
-  createdAt: string,
-  updatedAt: string,
+  __typename: "Post";
+  id: string;
+  name: string;
+  description?: string | null;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export type UpdatePostInput = {
-  id: string,
-  name?: string | null,
-  description?: string | null,
+  id: string;
+  name?: string | null;
+  description?: string | null;
 };
 
 export type DeletePostInput = {
-  id: string,
+  id: string;
 };
 
 export type ModelPostFilterInput = {
-  id?: ModelIDInput | null,
-  name?: ModelStringInput | null,
-  description?: ModelStringInput | null,
-  and?: Array< ModelPostFilterInput | null > | null,
-  or?: Array< ModelPostFilterInput | null > | null,
-  not?: ModelPostFilterInput | null,
+  id?: ModelIDInput | null;
+  name?: ModelStringInput | null;
+  description?: ModelStringInput | null;
+  and?: Array<ModelPostFilterInput | null> | null;
+  or?: Array<ModelPostFilterInput | null> | null;
+  not?: ModelPostFilterInput | null;
 };
 
 export type ModelIDInput = {
-  ne?: string | null,
-  eq?: string | null,
-  le?: string | null,
-  lt?: string | null,
-  ge?: string | null,
-  gt?: string | null,
-  contains?: string | null,
-  notContains?: string | null,
-  between?: Array< string | null > | null,
-  beginsWith?: string | null,
-  attributeExists?: boolean | null,
-  attributeType?: ModelAttributeTypes | null,
-  size?: ModelSizeInput | null,
+  ne?: string | null;
+  eq?: string | null;
+  le?: string | null;
+  lt?: string | null;
+  ge?: string | null;
+  gt?: string | null;
+  contains?: string | null;
+  notContains?: string | null;
+  between?: Array<string | null> | null;
+  beginsWith?: string | null;
+  attributeExists?: boolean | null;
+  attributeType?: ModelAttributeTypes | null;
+  size?: ModelSizeInput | null;
 };
 
 export type ModelPostConnection = {
-  __typename: "ModelPostConnection",
-  items:  Array<Post | null >,
-  nextToken?: string | null,
+  __typename: "ModelPostConnection";
+  items: Array<Post | null>;
+  nextToken?: string | null;
 };
 
 export type CreatePostMutationVariables = {
-  input: CreatePostInput,
-  condition?: ModelPostConditionInput | null,
+  input: CreatePostInput;
+  condition?: ModelPostConditionInput | null;
 };
 
 export type CreatePostMutation = {
-  createPost?:  {
-    __typename: "Post",
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+  createPost?: {
+    __typename: "Post";
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  } | null;
 };
 
 export type UpdatePostMutationVariables = {
-  input: UpdatePostInput,
-  condition?: ModelPostConditionInput | null,
+  input: UpdatePostInput;
+  condition?: ModelPostConditionInput | null;
 };
 
 export type UpdatePostMutation = {
-  updatePost?:  {
-    __typename: "Post",
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+  updatePost?: {
+    __typename: "Post";
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  } | null;
 };
 
 export type DeletePostMutationVariables = {
-  input: DeletePostInput,
-  condition?: ModelPostConditionInput | null,
+  input: DeletePostInput;
+  condition?: ModelPostConditionInput | null;
 };
 
 export type DeletePostMutation = {
-  deletePost?:  {
-    __typename: "Post",
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+  deletePost?: {
+    __typename: "Post";
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  } | null;
 };
 
 export type GetPostQueryVariables = {
-  id: string,
+  id: string;
 };
 
 export type GetPostQuery = {
-  getPost?:  {
-    __typename: "Post",
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+  getPost?: {
+    __typename: "Post";
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  } | null;
 };
 
 export type ListPostsQueryVariables = {
-  filter?: ModelPostFilterInput | null,
-  limit?: number | null,
-  nextToken?: string | null,
+  filter?: ModelPostFilterInput | null;
+  limit?: number | null;
+  nextToken?: string | null;
 };
 
 export type ListPostsQuery = {
-  listPosts?:  {
-    __typename: "ModelPostConnection",
-    items:  Array< {
-      __typename: "Post",
-      id: string,
-      name: string,
-      description?: string | null,
-      createdAt: string,
-      updatedAt: string,
-    } | null >,
-    nextToken?: string | null,
-  } | null,
+  listPosts?: {
+    __typename: "ModelPostConnection";
+    items: Array<{
+      __typename: "Post";
+      id: string;
+      name: string;
+      description?: string | null;
+      createdAt: string;
+      updatedAt: string;
+    } | null>;
+    nextToken?: string | null;
+  } | null;
 };
 
 export type OnCreatePostSubscription = {
-  onCreatePost?:  {
-    __typename: "Post",
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+  onCreatePost?: {
+    __typename: "Post";
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  } | null;
 };
 
 export type OnUpdatePostSubscription = {
-  onUpdatePost?:  {
-    __typename: "Post",
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+  onUpdatePost?: {
+    __typename: "Post";
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  } | null;
 };
 
 export type OnDeletePostSubscription = {
-  onDeletePost?:  {
-    __typename: "Post",
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+  onDeletePost?: {
+    __typename: "Post";
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  } | null;
 };
