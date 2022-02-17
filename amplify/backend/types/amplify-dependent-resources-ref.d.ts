e31ee10f69f207ec3838ca623b124b0b156730de
Isaac Farciert <rfisaac2296@yahoo.com>
2022-02-17 17:14:14 -0500

    configure GraphQL API AppSync

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "awsamplifynext": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
